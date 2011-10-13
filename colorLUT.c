52a457ee39d2d9dea8799acbc8e2e2b5a727a795
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-10-13 16:17:56 +0200

    Okno

--- colorLUT.c
+++ colorLUT.c
@@ -1,5 +1,31 @@
+#include <stdbool.h>
+
 #include <SDL.h>
+#include <SDL_opengl.h>
 
 int main() {
-	printf("Hello World!\n");
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	
+	if (SDL_SetVideoMode(800, 600, 0, SDL_OPENGL) == NULL) {
+		fprintf(stderr, "Unable to create OpenGL screen: %s\n", SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+	
+	SDL_WM_SetCaption("colorLUT", NULL);
+	
+	bool done = false;
+	while (!done) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
+				done = true;
+			}
+		}
+	}
+	SDL_Quit();
+	exit();
 }
