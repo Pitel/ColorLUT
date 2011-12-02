6670cd666ef27815335479812d1a3749e1ce96da
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-12-02 17:04:34 +0100

    Upravy, zruseni vertex shaderu

--- colorLUT.c
+++ colorLUT.c
@@ -28,40 +28,33 @@ char *file2string(const char *path) {
 void shaderlog(GLuint obj) {
 	int infologLength = 0;
 	int maxLength;
-
+	
 	if(glIsShader(obj)) {
 		glGetShaderiv(obj,GL_INFO_LOG_LENGTH,&maxLength);
 	} else {
 		glGetProgramiv(obj,GL_INFO_LOG_LENGTH,&maxLength);
 	}
 	char infoLog[maxLength];
-
+	
 	if (glIsShader(obj)) {
 		glGetShaderInfoLog(obj, maxLength, &infologLength, infoLog);
 	} else {
 		glGetProgramInfoLog(obj, maxLength, &infologLength, infoLog);
 	}
-
+	
 	if (infologLength > 0) {
 		printf("%s\n", infoLog);
 	}
 }
 
-float vertices[8] = {-1,1, 1,1, 1,-1, -1,-1};
-
+const short vertices[] = {-1,1, 1,1, 1,-1, -1,-1};
 /* The main drawing function. */
-void DrawGLScene()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
+void DrawGLScene() {
+	glClear(GL_COLOR_BUFFER_BIT);
 	glEnableClientState(GL_VERTEX_ARRAY);
-
-	glVertexPointer(2,GL_FLOAT,0,vertices);
-
-    // Draw points
-    glDrawArrays(GL_QUADS, 0, 8);
-
-    SDL_GL_SwapBuffers();
+	glVertexPointer(2, GL_SHORT, 0, vertices);
+	glDrawArrays(GL_QUADS, 0, 8);
+	SDL_GL_SwapBuffers();
 }
 
 
@@ -71,56 +64,47 @@ int main() {
 		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
-
+	
 	if (SDL_SetVideoMode(800, 600, 0, SDL_OPENGL) == NULL) {
 		fprintf(stderr, "Unable to create OpenGL screen: %s\n", SDL_GetError());
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
-
+	
 	SDL_WM_SetCaption("colorLUT", NULL);
-
+	
 	GLenum err = glewInit();
 	if (err != GLEW_OK) {
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
-
+	
 	printf("Using GLEW Version %s\n", glewGetString(GLEW_VERSION));
-
+	
 	if (!GLEW_VERSION_2_0) {
 		fprintf(stderr, "This machine sucks and does not support OpenGL 2.0!\n");
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
-
-	const char* vs_src = file2string("colorLUT.vs");
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER_ARB);
-	glShaderSource(vs, 1, &vs_src, NULL);
-	glCompileShader(vs);
-	shaderlog(vs);
-
+	
 	const char* fs_src = file2string("colorLUT.fs");
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER_ARB);
 	glShaderSource(fs, 1, &fs_src, NULL);
 	glCompileShader(fs);
 	shaderlog(fs);
-
+	
 	GLuint program = glCreateProgram();
-	glAttachShader(program, vs);
 	glAttachShader(program, fs);
 	glLinkProgram(program);
 	shaderlog(program);
-
+	
 	glUseProgram(program);
-
+	
 	bool done = false;
 	SDL_Event event;
 	while (!done && SDL_WaitEvent(&event)) {
-
-        DrawGLScene();
-
+		DrawGLScene();
 		if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
 			done = true;
 		}
--- colorLUT.vs
+++
@@ -1,7 +0,0 @@
-#version 120
-
-//in vec2 position;
-
-void main() {
-    gl_Position = vec4(gl_Vertex.xy,0,1);
-}
